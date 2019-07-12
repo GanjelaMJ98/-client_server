a15d52bb668ff39904b338323a863e194e8c23eb
pavel <ganjela98@gmail.com>
2019-07-12 13:28:43 +0300

    Added error handling, Makefile
    Signed-off-by: pavel <ganjela98@gmail.com>

--- client.c
+++ client.c
@@ -3,6 +3,8 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <unistd.h> 		//close()
+#include <errno.h>
 
 char message[] = "Hello\n";
 char buf[sizeof(message)];
@@ -12,14 +14,21 @@ int main(){
 	struct sockaddr_in addr;
 
 	sock = socket(AF_INET, SOCK_STREAM, 0);
-
+	if(sock < 0){
+		perror("socket");
+		exit(1);
+	}
 	addr.sin_family = AF_INET;
-	addr.sin_addr.s_addr = INADDR_LOOPBACK;
+	addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
 	addr.sin_port = htons(3333);
 
-	connect(sock, (struct sockaddr*)&addr, sizeof(addr));
-
+	if(connect(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0){
+		perror("connect");
+		exit(2);
+	}
+	printf("Sending...\n");
 	send(sock, message, sizeof(message), 0);
+	printf("Reception...\n");
 	recv(sock, buf, sizeof(message), 0);
 
 	printf("%s\n", buf);
--- server.c
+++ server.c
@@ -2,6 +2,8 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <unistd.h> 		//close()
+#include <errno.h>
 
 int main(int argc, char const *argv[]){
 	int sock;
@@ -11,16 +13,28 @@ int main(int argc, char const *argv[]){
 	struct sockaddr_in serv_addr;
 
 	listener = socket(AF_INET, SOCK_STREAM, 0);
+	if(listener < 0) {
+		perror("socket");
+		exit(1);
+	}
 
 	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 	serv_addr.sin_port = htons(3333);
 
-	bind(listener, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+	if(bind(listener, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0){
+		perror("bind");
+		exit(2);
+	}
+
 	listen(listener,1);
 
 	while(1){
 		sock = accept(listener, NULL, NULL);
+		if(sock < 0){
+			perror("accept");
+			exit(3);
+		}
 		while(1){
 			bytes_read = recv(sock, buf, 1024, 0);
 			if(bytes_read <= 0) break;
